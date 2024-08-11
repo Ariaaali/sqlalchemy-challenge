a7d17b08cb33f567914d65b3604ce05cca44e55a
Aria Li <oreo04@163.com>
2024-08-11 23:03:46 +1000

    adding correct file

--- README.md
+++ README.md
@@ -1,4 +1,3 @@
 # sqlalchemy-challenge
 sqlalchemy-challenge homework
-Please give me some extra time to load the rest of the assignment
-My folder is not uploading
+*one of the tutor help debugged one line of the code in api route. 
--- Surfsup/app.py
+++ Surfsup/app.py
@@ -1,6 +1,7 @@
 # Import the dependencies.
 from sqlalchemy import func
 import numpy as np
+import datetime as dt
 from sqlalchemy import create_engine, inspect
 from flask import Flask, jsonify 
 from sqlalchemy.ext.automap import automap_base
@@ -39,7 +40,7 @@ def home():
         f"/api/v1.0/stations <br/>"
         f"/api/v1.0/tobs <br/>"
         f"/api/v1.0/<start> <br/>"
-        f"//api/v1.0/<start>/<end>"
+        f"/api/v1.0/<start>/<end>"
     )
 
 #################################################
@@ -62,11 +63,44 @@ def precipitation():
 @app.route("/api/v1.0/stations")
 def station():
     """Return the list of station"""
-    station_query=session.query(Measurement.station).all()
+    station_query=session.query(Measurement.station).group_by(Measurement.station).all()
     session.close()
     station_names = list(np.ravel(station_query))
     return jsonify(station_names)
-  
+
+@app.route("/api/v1.0/tobs")
+def tobs():
+    """return the dates and temperature observations of the most-active station for the previous year of data"""
+    active_station=session.query(Measurement.date,Measurement.tobs, func.count(Measurement.station)).\
+    filter(Measurement.date.between('2016-08-23' , '2017-08-23')).\
+    group_by(Measurement.station).order_by(Measurement.station.desc()).first()
+    session.close()
+    active_station_name = list(np.ravel(active_station))
+    return jsonify(active_station_name)
+
+@app.route("/api/v1.0/<start>")
+@app.route("/api/v1.0/<start>/<end>")
+def date(start= None, end=None):
+    selection= [func.min(Measurement.tobs), func.max(Measurement.tobs), func.avg(Measurement.tobs)]
+    if not end:
+        start = dt.datetime.strptime(start, "%m%d%Y")
+        results = session.query(*selection).filter(Measurement.date >= start).all()
+        session.close()
+        temps = list(np.ravel(results))
+        return jsonify(temps)
+
+    
+    start = dt.datetime.strptime(start, "%m%d%Y")
+    end = dt.datetime.strptime(start, "%m%d%Y")
+
+    results = session.query(*selection).filter(Measurement.date >= start).filter(Measurement.date <= end).all()
+    session.close()
+    temps = list(np.ravel(results))
+    return jsonify(temps)
+    
+    
+
+    
 #################################################
 if __name__ == '__main__':
     app.run(debug=True)
